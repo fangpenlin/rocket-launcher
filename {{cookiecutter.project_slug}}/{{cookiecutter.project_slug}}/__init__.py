bc611ace00be2711fe9d5cac1bcf4d8da77b8682
Fang-Pen Lin <hello@fangpenlin.com>
2018-09-09 22:55:52 -0700

    Fix imports

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 from flask.helpers import get_debug_flag
 
-from {{cookiecutter.project_slug}}.app import create_app
-from {{cookiecutter.project_slug}}.settings import DevConfig
-from {{cookiecutter.project_slug}}.settings import ProdConfig
+from .app import create_app
+from .settings import DevConfig
+from .settings import ProdConfig
 
 CONFIG = DevConfig if get_debug_flag() else ProdConfig
 
